744d1c0330a28feeb0c402ed336f22eafbbce229
CarlAurelius <carl@nextixsystems.com>
2014-06-30 14:56:17 +0800

    Customer Contact functionalities add and edit

--- src/co/nextix/jardine/customers/CustomerConstants.java
+++ src/co/nextix/jardine/customers/CustomerConstants.java
@@ -3,6 +3,8 @@ package co.nextix.jardine.customers;
 public class CustomerConstants {
 	
 	public static String KEY_CUSTOMER_LONG_ID = "CustomerID";
+	public static String KEY_CUSTOMER_USERNAME = "CustomerUsername";
+	public static String CUSTOMER_NAME = "";
 	public static long CUSTOMER_ID = 0;
 	public static String ADD_CUSTOMER_CONTACT = "Add Customer Contact";
 }
