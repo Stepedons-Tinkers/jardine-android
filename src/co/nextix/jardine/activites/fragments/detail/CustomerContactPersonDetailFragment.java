095c01b2996c826b18b4e22d320b5e5bfaac501c
francis@nextixsystems.com <francis@nextixsystems.com>
2014-07-27 17:34:56 +0800

    Commit : CustomerContactPerson (Detailed Data)

--- src/co/nextix/jardine/activites/fragments/detail/CustomerContactPersonDetailFragment.java
+++ src/co/nextix/jardine/activites/fragments/detail/CustomerContactPersonDetailFragment.java
@@ -1,6 +1,5 @@
 package co.nextix.jardine.activites.fragments.detail;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,29 +10,60 @@ import android.widget.Button;
 import android.widget.TextView;
 import co.nextix.jardine.JardineApp;
 import co.nextix.jardine.R;
-import co.nextix.jardine.database.records.ActivityRecord;
+import co.nextix.jardine.database.records.CustomerContactRecord;
+import co.nextix.jardine.database.records.CustomerRecord;
+import co.nextix.jardine.database.records.PicklistRecord;
+import co.nextix.jardine.database.records.UserRecord;
+import co.nextix.jardine.database.tables.CustomerContactTable;
+import co.nextix.jardine.database.tables.CustomerTable;
+import co.nextix.jardine.database.tables.UserTable;
+import co.nextix.jardine.database.tables.picklists.PCustConPositionTable;
 
 public class CustomerContactPersonDetailFragment extends Fragment {
 
-	private ActivityRecord activityRecord = null;
-	private SharedPreferences pref = null;
+	private Bundle bundle;
+	private long customer_id;
+	private CustomerContactRecord record;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
 		View myFragmentView = inflater.inflate(R.layout.fragment_activity_detail_customer_contact_person, container, false);
-		this.pref = getActivity().getApplicationContext().getSharedPreferences("ActivityInfo", 0);
-		this.activityRecord = JardineApp.DB.getActivity().getById(pref.getLong("activity_id", 0000));
 	
-		((TextView) myFragmentView.findViewById(R.id.crm_no)).setText(this.activityRecord.getCrm());
-		((TextView) myFragmentView.findViewById(R.id.first_name)).setText(this.activityRecord.getCheckIn());
-		((TextView) myFragmentView.findViewById(R.id.last_name)).setText(this.activityRecord.getCheckOut());
-		((TextView) myFragmentView.findViewById(R.id.position)).setText(String.valueOf(this.activityRecord.getLatitude()));
-		((TextView) myFragmentView.findViewById(R.id.mobile_no)).setText(String.valueOf(this.activityRecord.getLongitude()));
+		bundle = getArguments();
+		if(bundle != null){
+			customer_id = bundle.getLong("customer_id", 0);
+		}		
+		CustomerContactTable contact = JardineApp.DB.getCustomerContact();
+		record = contact.getById(customer_id);
+		
+		((TextView) myFragmentView.findViewById(R.id.crm_no)).setText(this.record.getCrm());
+		((TextView) myFragmentView.findViewById(R.id.first_name)).setText(this.record.getFirstName());
+		((TextView) myFragmentView.findViewById(R.id.last_name)).setText(this.record.getLastName());
+		
+		PCustConPositionTable customer = JardineApp.DB.getCustomerContactPosition();
+		if(customer != null){
+			PicklistRecord customerRecord = customer.getById(this.record.getPosition());
+			((TextView) myFragmentView.findViewById(R.id.position)).setText("");
+			if(customerRecord != null){
+				((TextView) myFragmentView.findViewById(R.id.position)).setText(customerRecord.toString());
+			}
+		}
+		
+		((TextView) myFragmentView.findViewById(R.id.mobile_no)).setText(String.valueOf(this.record.getMobileNo()));
+
+		UserTable user = JardineApp.DB.getUser();
+		if(user != null){
+			UserRecord userRecord = user.getById(this.record.getCreatedBy());
+			((TextView) myFragmentView.findViewById(R.id.created_by)).setText("");
+			if(userRecord != null){
+				((TextView) myFragmentView.findViewById(R.id.created_by)).setText(userRecord.toString());
+			}
+		}
+		
 //		((TextView) myFragmentView.findViewById(R.id.product_brand)).setText(this.activityRecord.getNotes());
 //		((TextView) myFragmentView.findViewById(R.id.created_time)).setText(this.activityRecord.getModifiedTime());
 //		((TextView) myFragmentView.findViewById(R.id.modified_time)).setText(this.activityRecord.getModifiedTime());
-		((TextView) myFragmentView.findViewById(R.id.created_by)).setText("getCompetitorActivities()");
 //		((TextView) myFragmentView.findViewById(R.id.other_remarks)).setText("getCompetitorActivities()");
 //		((Button) myFragmentView.findViewById(R.id.back_button)).setOnClickListener(new OnClickListener() {
 //			
