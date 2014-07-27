af39c86f0ca474f73417b1459df2cff18abdbb7e
Christian P. Lumapac <christianlumapac@yahoo.com>
2014-07-27 18:10:48 +0800

    Adding activity functionality level 3

--- src/co/nextix/jardine/activities/add/fragments/AddActivityProductSupplierFragment.java
+++ src/co/nextix/jardine/activities/add/fragments/AddActivityProductSupplierFragment.java
@@ -1,17 +1,94 @@
 package co.nextix.jardine.activities.add.fragments;
 
+import android.animation.ValueAnimator;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 import co.nextix.jardine.R;
+import co.nextix.jardine.database.records.ActivityRecord;
+import co.nextix.jardine.database.records.CustomerRecord;
+import co.nextix.jardine.database.records.ProductRecord;
+
+import com.dd.CircularProgressButton;
 
 public class AddActivityProductSupplierFragment extends Fragment {
 
+	private boolean flag = false;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.add_activity_product_supplier, container, false);
+		final View view = inflater.inflate(R.layout.add_activity_product_supplier, container, false);
+
+		((CircularProgressButton) view.findViewById(R.id.btnWithText1)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(final View v) {
+				if (((CircularProgressButton) v).getProgress() == 0) {
+
+					ValueAnimator widthAnimation = ValueAnimator.ofInt(1, 100);
+					widthAnimation.setDuration(1500);
+					widthAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+					widthAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+						@Override
+						public void onAnimationUpdate(ValueAnimator animation) {
+
+							Integer value = (Integer) animation.getAnimatedValue();
+							((CircularProgressButton) v).setProgress(value);
+
+							if (!flag) {
+
+								((CircularProgressButton) v).setProgress(-1);
+							}
+						}
+					});
+
+					widthAnimation.start();
+
+					long activity = ((ActivityRecord) ((Spinner) view.findViewById(R.id.activity)).getSelectedItem()).getId();
+					long productBrand = ((ProductRecord) ((Spinner) view.findViewById(R.id.product_brand)).getSelectedItem()).getId();
+					long supplier = ((CustomerRecord) ((Spinner) view.findViewById(R.id.supplier)).getSelectedItem()).getId();
+					String otherRemarks = ((EditText) view.findViewById(R.id.other_remarks)).getText().toString();
+
+					/** Checking of required fields **/
+					SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("ActivityInfo", 0);
+
+					flag = true;
+					Editor editor = pref.edit();
+					editor.putLong("activity", activity);
+					editor.putLong("product_brand", productBrand);
+					editor.putLong("supplier", supplier);
+					editor.putString("product_supplier_other_remarks", otherRemarks);
+					editor.commit(); // commit changes
+
+					Handler handler = new Handler();
+					handler.postDelayed(new Runnable() {
+
+						@Override
+						public void run() {
+							getFragmentManager().popBackStackImmediate();
+						}
+
+					}, 2700);
+
+				} else {
+
+					// Code here
+					((CircularProgressButton) v).setProgress(0);
+					//transition
+				}
+			}
+		});
+
 		return view;
 	}
 }
--- src/co/nextix/jardine/activities/add/fragments/AddCompetitorStockCheckFragment.java
+++ src/co/nextix/jardine/activities/add/fragments/AddCompetitorStockCheckFragment.java
@@ -1,24 +1,98 @@
 package co.nextix.jardine.activities.add.fragments;
 
+import android.animation.ValueAnimator;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import co.nextix.jardine.R;
+import co.nextix.jardine.database.records.CompetitorProductRecord;
+import co.nextix.jardine.database.records.PicklistRecord;
+
+import com.dd.CircularProgressButton;
 
 public class AddCompetitorStockCheckFragment extends Fragment {
 	
+	private boolean flag = false;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View myFragmentView = inflater.inflate(
+		final View view = inflater.inflate(
 				R.layout.fragment_activity_add_competitor_product_stock_check,
 				container, false);
+		
+		((CircularProgressButton) view.findViewById(R.id.btnWithText1)).setOnClickListener(new OnClickListener() {
 
-		return myFragmentView;
+			@Override
+			public void onClick(final View v) {
+				if (((CircularProgressButton) v).getProgress() == 0) {
+
+					ValueAnimator widthAnimation = ValueAnimator.ofInt(1, 100);
+					widthAnimation.setDuration(1500);
+					widthAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+					widthAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+						@Override
+						public void onAnimationUpdate(ValueAnimator animation) {
+
+							Integer value = (Integer) animation.getAnimatedValue();
+							((CircularProgressButton) v).setProgress(value);
+
+							if (!flag) {
+
+								((CircularProgressButton) v).setProgress(-1);
+							}
+						}
+					});
+
+					widthAnimation.start();
+
+					long competitorProduct = ((CompetitorProductRecord) ((Spinner) view.findViewById(R.id.competitor_product)).getSelectedItem()).getId();
+					long stockStatus = ((PicklistRecord) ((Spinner) view.findViewById(R.id.stock_status)).getSelectedItem()).getId();
+					int loadedOnShelves = ((CheckBox) view.findViewById(R.id.loaded_on_shelves)).isChecked() ? 1 : 0;
+					String otherRemarks = ((EditText) view.findViewById(R.id.other_remarks)).getText().toString();
+
+					/** Checking of required fields **/
+					SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("ActivityInfo", 0);
+
+					flag = true;
+					Editor editor = pref.edit();
+					editor.putLong("competitor_product", competitorProduct);
+					editor.putLong("stock_status", stockStatus);
+					editor.putInt("loaded_on_shelves", loadedOnShelves);
+					editor.putString("competitor_other_remarks", otherRemarks);
+					editor.commit(); // commit changes
+
+					Handler handler = new Handler();
+					handler.postDelayed(new Runnable() {
+
+						@Override
+						public void run() {
+							getFragmentManager().popBackStackImmediate();
+						}
+
+					}, 2700);
+
+				} else {
+
+					// Code here
+					((CircularProgressButton) v).setProgress(0);
+					//transition
+				}
+			}
+		});
+
+		return view;
 	}
 
 }
