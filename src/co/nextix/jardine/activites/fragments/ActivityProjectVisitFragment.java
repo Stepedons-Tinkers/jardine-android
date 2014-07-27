d9bb60f33dda7b9b86a4fcc53f5bbed2302ba7df
francis@nextixsystems.com <francis@nextixsystems.com>
2014-07-27 19:32:00 +0800

    Commit : ActivityAdminWorks, ActivityProjectVisit -> not fixed data

--- src/co/nextix/jardine/activites/fragments/ActivityProjectVisitFragment.java
+++ src/co/nextix/jardine/activites/fragments/ActivityProjectVisitFragment.java
@@ -1,16 +1,22 @@
 package co.nextix.jardine.activites.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import co.nextix.jardine.JardineApp;
 import co.nextix.jardine.R;
+import co.nextix.jardine.database.records.ActivityRecord;
 
 public class ActivityProjectVisitFragment extends Fragment {
 	private static volatile ActivityProjectVisitFragment instance = null;
 	private boolean flag = false;
-
+	private ActivityRecord activityRecord = null;
+	private SharedPreferences pref = null;
+	
 	public static ActivityProjectVisitFragment getInstance() {
 		if (instance == null) {
 			synchronized (ActivityProjectVisitFragment.class) {
@@ -26,8 +32,14 @@ public class ActivityProjectVisitFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.view_activity_project_visit, container, false);
-
-
+		this.pref = getActivity().getApplicationContext().getSharedPreferences(
+				"ActivityInfo", 0);
+		this.activityRecord = JardineApp.DB.getActivity().getById(
+				pref.getLong("activity_id", 0000));
+		
+		((TextView) rootView.findViewById(R.id.project_name))
+		.setText(this.activityRecord.getProjectName());
+		
 		return rootView;
 	}
 }
