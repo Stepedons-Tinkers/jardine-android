e15362cb3ecd3083282d38f78556b78b968cbff1
francis@nextixsystems.com <francis@nextixsystems.com>
2014-07-27 20:17:59 +0800

    Commit : brandactivationfragment and trainingfragment not data

--- src/co/nextix/jardine/activites/fragments/ActivityBrandActivationFragment.java
+++ src/co/nextix/jardine/activites/fragments/ActivityBrandActivationFragment.java
@@ -1,15 +1,21 @@
 package co.nextix.jardine.activites.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import co.nextix.jardine.JardineApp;
 import co.nextix.jardine.R;
+import co.nextix.jardine.database.records.ActivityRecord;
 
 public class ActivityBrandActivationFragment extends Fragment {
 	private static volatile ActivityBrandActivationFragment instance = null;
 	private boolean flag = false;
+	private ActivityRecord activityRecord = null;
+	private SharedPreferences pref = null;
 
 	public static ActivityBrandActivationFragment getInstance() {
 		if (instance == null) {
@@ -26,7 +32,13 @@ public class ActivityBrandActivationFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.view_activity_brand_activation, container, false);
-
+		this.pref = getActivity().getApplicationContext().getSharedPreferences(
+				"ActivityInfo", 0);
+		this.activityRecord = JardineApp.DB.getActivity().getById(
+				pref.getLong("activity_id", 0000));
+		
+		((TextView) rootView.findViewById(R.id.end_user_activity_type))
+		.setText(this.activityRecord.getEndUserActivityTypes());
 
 		return rootView;
 	}
--- src/co/nextix/jardine/activites/fragments/ActivityTrainingsFragment.java
+++ src/co/nextix/jardine/activites/fragments/ActivityTrainingsFragment.java
@@ -1,16 +1,21 @@
 package co.nextix.jardine.activites.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import co.nextix.jardine.JardineApp;
 import co.nextix.jardine.R;
+import co.nextix.jardine.database.records.ActivityRecord;
 
 public class ActivityTrainingsFragment extends Fragment {
 	private static volatile ActivityTrainingsFragment instance = null;
 	private boolean flag = false;
-
+	private ActivityRecord activityRecord = null;
+	private SharedPreferences pref = null;
+	
 	public static ActivityTrainingsFragment getInstance() {
 		if (instance == null) {
 			synchronized (ActivityTrainingsFragment.class) {
@@ -26,7 +31,10 @@ public class ActivityTrainingsFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.view_activity_trainings, container, false);
-
+		this.pref = getActivity().getApplicationContext().getSharedPreferences(
+				"ActivityInfo", 0);
+		this.activityRecord = JardineApp.DB.getActivity().getById(
+				pref.getLong("activity_id", 0000));
 
 		return rootView;
 	}
