fb903afff054db2762c890309b657ed82eca35e1
Christian P. Lumapac <christianlumapac@yahoo.com>
2014-07-27 21:47:21 +0800

    Merge changes on add activity functionality

--- src/co/nextix/jardine/activities/add/fragments/AddActivityDetailsAndNotesFragment.java
+++ src/co/nextix/jardine/activities/add/fragments/AddActivityDetailsAndNotesFragment.java
@@ -1,8 +1,11 @@
 package co.nextix.jardine.activities.add.fragments;
 
+import java.util.List;
+
 import android.animation.ValueAnimator;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.hardware.Camera.Area;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -11,11 +14,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import co.nextix.jardine.JardineApp;
 import co.nextix.jardine.R;
 import co.nextix.jardine.database.records.CityTownRecord;
 import co.nextix.jardine.database.records.CustomerRecord;
@@ -26,11 +31,34 @@ import co.nextix.jardine.database.records.WorkplanEntryRecord;
 import com.dd.CircularProgressButton;
 
 public class AddActivityDetailsAndNotesFragment extends Fragment {
+	private ArrayAdapter<CustomerRecord> customerAdapter = null;
+	private ArrayAdapter<PicklistRecord> areaAdapter = null;
+	private ArrayAdapter<ProvinceRecord> provinceAdapter = null;
+	private ArrayAdapter<CityTownRecord> cityTownAdapter = null;
+	private ArrayAdapter<WorkplanEntryRecord> workplanEntryAdapter = null;
+
 	private boolean trapping = false;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		List<CustomerRecord> customer = JardineApp.DB.getCustomer().getAllRecords();
+		List<PicklistRecord> area = JardineApp.DB.getArea().getAllRecords();
+		List<ProvinceRecord> province = JardineApp.DB.getProvince().getAllRecords();
+		List<CityTownRecord> cityTown = JardineApp.DB.getCityTown().getAllRecords();
+		List<WorkplanEntryRecord> workplanEntry = JardineApp.DB.getWorkplanEntry().getAllRecords();
+		
+		this.customerAdapter = new ArrayAdapter<CustomerRecord>(getActivity().getApplicationContext(), R.layout.add_activity_textview, customer);
+		this.areaAdapter = new ArrayAdapter<PicklistRecord>(getActivity().getApplicationContext(), R.layout.add_activity_textview, area);
+		this.provinceAdapter = new ArrayAdapter<ProvinceRecord>(getActivity().getApplicationContext(), R.layout.add_activity_textview, province);
+		this.cityTownAdapter = new ArrayAdapter<CityTownRecord>(getActivity().getApplicationContext(), R.layout.add_activity_textview, cityTown);
+		this.workplanEntryAdapter = new ArrayAdapter<>(getActivity().getApplicationContext(), R.layout.add_activity_textview, workplanEntry);
+
 		final View view = inflater.inflate(R.layout.add_activity_activity_details_and_notes, container, false);
+		((Spinner) view.findViewById(R.id.customer)).setAdapter(this.customerAdapter);
+		((Spinner) view.findViewById(R.id.area)).setAdapter(this.areaAdapter);
+		((Spinner) view.findViewById(R.id.province)).setAdapter(this.provinceAdapter);
+		((Spinner) view.findViewById(R.id.city_town)).setAdapter(this.cityTownAdapter);
+		((Spinner) view.findViewById(R.id.workplan_entry)).setAdapter(this.workplanEntryAdapter);
 
 		((CircularProgressButton) view.findViewById(R.id.btnWithText1)).setOnClickListener(new OnClickListener() {
 
@@ -89,7 +117,7 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 						editor.putString("notes", notes);
 						editor.putString("next_steps", nextSteps);
 						editor.putString("follow_up_committment_date", followUpCommittmentDate);
-						
+
 						editor.commit(); // commit changes
 
 					} else {
@@ -111,12 +139,14 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 				} else {
 
 					((CircularProgressButton) v).setProgress(0);
-					/*addActFrag.pager.setCurrentItem(tabID);
-
-					ft = getActivity().getSupportFragmentManager().beginTransaction();
-					ft.replace(frag_layout_id, fragmentForTransition);
-					ft.addToBackStack(null);
-					ft.commit();*/
+					/*
+					 * addActFrag.pager.setCurrentItem(tabID);
+					 * 
+					 * ft =
+					 * getActivity().getSupportFragmentManager().beginTransaction
+					 * (); ft.replace(frag_layout_id, fragmentForTransition);
+					 * ft.addToBackStack(null); ft.commit();
+					 */
 				}
 			}
 		});
