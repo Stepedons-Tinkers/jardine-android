102cff05e2968b8f2a15e81f4ccf951de9a00343
Christian P. Lumapac <christianlumapac@yahoo.com>
2014-07-28 18:34:41 +0800

    Bug fixing

--- src/co/nextix/jardine/activities/add/fragments/AddActivityDetailsAndNotesFragment.java
+++ src/co/nextix/jardine/activities/add/fragments/AddActivityDetailsAndNotesFragment.java
@@ -1,8 +1,11 @@
 package co.nextix.jardine.activities.add.fragments;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import android.animation.ValueAnimator;
+import android.app.DatePickerDialog;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
@@ -14,8 +17,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -28,6 +34,8 @@ import co.nextix.jardine.database.records.CustomerRecord;
 import co.nextix.jardine.database.records.PicklistRecord;
 import co.nextix.jardine.database.records.ProvinceRecord;
 import co.nextix.jardine.database.records.WorkplanEntryRecord;
+import co.nextix.jardine.database.tables.picklists.PCityTownTable;
+import co.nextix.jardine.database.tables.picklists.PProvinceTable;
 
 import com.dd.CircularProgressButton;
 
@@ -37,6 +45,7 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 
 	private Fragment fragmentForTransition;
 
+	private View rootView = null;
 	private ArrayAdapter<CustomerRecord> customerAdapter = null;
 	private ArrayAdapter<PicklistRecord> areaAdapter = null;
 	private ArrayAdapter<ProvinceRecord> provinceAdapter = null;
@@ -49,8 +58,18 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 	private int frag_layout_id;
 	private Fragment fragment = null;
 	private AddActivityFragment addActFrag;
+	private Calendar calendar = null;
+	
+	private String formattedDate = null;
+	private int day = 0;
+	private int month = 0;
+	private int year = 0;
 
 	public AddActivityDetailsAndNotesFragment(Fragment frag) {
+		this.calendar = Calendar.getInstance();
+		this.day = this.calendar.get(Calendar.DAY_OF_MONTH);
+		this.month = this.calendar.get(Calendar.MONTH);
+		this.year = this.calendar.get(Calendar.YEAR);
 		this.fragment = frag;
 	}
 
@@ -82,12 +101,57 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 		this.workplanEntryAdapter = new ArrayAdapter<WorkplanEntryRecord>(getActivity().getApplicationContext(),
 				R.layout.add_activity_textview, workplanEntry);
 
-		final View view = inflater.inflate(R.layout.add_activity_activity_details_and_notes, container, false);
-		((Spinner) view.findViewById(R.id.customer)).setAdapter(this.customerAdapter);
-		((Spinner) view.findViewById(R.id.area)).setAdapter(this.areaAdapter);
-		((Spinner) view.findViewById(R.id.province)).setAdapter(this.provinceAdapter);
-		((Spinner) view.findViewById(R.id.city_town)).setAdapter(this.cityTownAdapter);
-		((Spinner) view.findViewById(R.id.workplan_entry)).setAdapter(this.workplanEntryAdapter);
+		this.rootView = inflater.inflate(R.layout.add_activity_activity_details_and_notes, container, false);
+
+		((Spinner) this.rootView.findViewById(R.id.area)).setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View rootView, int position, long id) {
+				PProvinceTable provinceTable = JardineApp.DB.getProvince();
+				provinceAdapter = new ArrayAdapter<ProvinceRecord>(JardineApp.context, R.layout.add_activity_textview, provinceTable
+						.getRecordsByAreaId(id + 1));
+				((Spinner) rootView.findViewById(R.id.province)).setAdapter(provinceAdapter);
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				Toast.makeText(getActivity().getApplicationContext(), "Must be filled!", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		((Spinner) this.rootView.findViewById(R.id.province)).setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View rootView, int position, long id) {
+				PCityTownTable cityTable = JardineApp.DB.getCityTown();
+				cityTownAdapter = new ArrayAdapter<CityTownRecord>(JardineApp.context, R.layout.add_activity_textview, cityTable
+						.getRecordsByProvinceId(id + 1));
+				((Spinner) rootView.findViewById(R.id.city_town)).setAdapter(cityTownAdapter);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				Toast.makeText(getActivity().getApplicationContext(), "Must be filled!", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		((TextView) this.rootView.findViewById(R.id.follow_up_commitment_date)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				DatePickerDialog pickDialog = new DatePickerDialog(getActivity(), android.R.style.Theme_Holo_Panel, datePickerListener,
+						year, month, day);
+				pickDialog.show();
+
+			}
+		});
+
+		((Spinner) rootView.findViewById(R.id.customer)).setAdapter(this.customerAdapter);
+		((Spinner) rootView.findViewById(R.id.area)).setAdapter(this.areaAdapter);
+		((Spinner) rootView.findViewById(R.id.province)).setAdapter(this.provinceAdapter);
+		((Spinner) rootView.findViewById(R.id.city_town)).setAdapter(this.cityTownAdapter);
+		((Spinner) rootView.findViewById(R.id.workplan_entry)).setAdapter(this.workplanEntryAdapter);
 
 		bundle = getArguments();
 
@@ -98,7 +162,7 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 		addActFrag = (AddActivityFragment) fragment;
 		// addActFrag.pager.setCurrentItem(4);
 
-		((CircularProgressButton) view.findViewById(R.id.btnWithText1)).setOnClickListener(new OnClickListener() {
+		((CircularProgressButton) rootView.findViewById(R.id.btnWithText1)).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(final View v) {
@@ -124,20 +188,21 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 
 					widthAnimation.start();
 
-					long customer = ((CustomerRecord) ((Spinner) view.findViewById(R.id.customer)).getSelectedItem()).getId();
-					long area = ((PicklistRecord) ((Spinner) view.findViewById(R.id.area)).getSelectedItem()).getId();
-					long province = ((ProvinceRecord) ((Spinner) view.findViewById(R.id.province)).getSelectedItem()).getId();
-					long cityTown = ((CityTownRecord) ((Spinner) view.findViewById(R.id.city_town)).getSelectedItem()).getId();
-					String objective = ((EditText) view.findViewById(R.id.objective)).getText().toString();
-					long workplanEntry = ((WorkplanEntryRecord) ((Spinner) view.findViewById(R.id.workplan_entry)).getSelectedItem())
+					long customer = ((CustomerRecord) ((Spinner) rootView.findViewById(R.id.customer)).getSelectedItem()).getId();
+					long area = ((PicklistRecord) ((Spinner) rootView.findViewById(R.id.area)).getSelectedItem()).getId();
+					long province = ((ProvinceRecord) ((Spinner) rootView.findViewById(R.id.province)).getSelectedItem()).getId();
+					long cityTown = ((CityTownRecord) ((Spinner) rootView.findViewById(R.id.city_town)).getSelectedItem()).getId();
+					String objective = ((EditText) rootView.findViewById(R.id.objective)).getText().toString();
+					long workplanEntry = ((WorkplanEntryRecord) ((Spinner) rootView.findViewById(R.id.workplan_entry)).getSelectedItem())
 							.getId();
-					int firstTimeVisit = ((CheckBox) view.findViewById(R.id.first_time_visit_checkbox)).isChecked() ? 1 : 0;
-					int plannedTimeVisit = ((CheckBox) view.findViewById(R.id.planned_visit_checkbox)).isChecked() ? 1 : 0;
+					int firstTimeVisit = ((CheckBox) rootView.findViewById(R.id.first_time_visit_checkbox)).isChecked() ? 1 : 0;
+					int plannedTimeVisit = ((CheckBox) rootView.findViewById(R.id.planned_visit_checkbox)).isChecked() ? 1 : 0;
 
-					String highlights = ((EditText) view.findViewById(R.id.highlights)).getText().toString();
-					String notes = ((EditText) view.findViewById(R.id.notes)).getText().toString();
-					String nextSteps = ((EditText) view.findViewById(R.id.next_steps)).getText().toString();
-					String followUpCommittmentDate = ((TextView) view.findViewById(R.id.follow_up_commitment_date)).getText().toString();
+					String highlights = ((EditText) rootView.findViewById(R.id.highlights)).getText().toString();
+					String notes = ((EditText) rootView.findViewById(R.id.notes)).getText().toString();
+					String nextSteps = ((EditText) rootView.findViewById(R.id.next_steps)).getText().toString();
+					String followUpCommittmentDate = ((TextView) rootView.findViewById(R.id.follow_up_commitment_date)).getText()
+							.toString();
 
 					/** Checking of required fields **/
 					SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("ActivityInfo", 0);
@@ -212,6 +277,23 @@ public class AddActivityDetailsAndNotesFragment extends Fragment {
 			}
 		});
 
-		return view;
+		return rootView;
+	}
+
+	protected DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+
+		@Override
+		public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+			year = selectedYear;
+			month = selectedMonth;
+			day = selectedDay;
+			formattedDate = year + "-" + FormatDateAndDay((month + 1)) + "-" + FormatDateAndDay(day);
+			((TextView) rootView.findViewById(R.id.follow_up_commitment_date)).setText(formattedDate);
+		}
+	};
+
+	protected String FormatDateAndDay(int digit) {
+		String formattedStringDigit = digit < 10 ? "0" + String.valueOf(digit) : String.valueOf(digit);
+		return String.valueOf(formattedStringDigit);
 	}
 }
