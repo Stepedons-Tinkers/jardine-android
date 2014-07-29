4e7a93f2a653e5968d52af81f33fe78ae6ddfeda
Christian P. Lumapac <christianlumapac@yahoo.com>
2014-07-29 18:46:45 +0800

    Fix Adding JDIProductStockCheck in activity

--- src/co/nextix/jardine/activities/add/fragments/AddJDIProductStockFragment.java
+++ src/co/nextix/jardine/activities/add/fragments/AddJDIProductStockFragment.java
@@ -1,5 +1,7 @@
 package co.nextix.jardine.activities.add.fragments;
 
+import java.util.List;
+
 import android.animation.ValueAnimator;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -12,12 +14,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import co.nextix.jardine.JardineApp;
 import co.nextix.jardine.R;
+import co.nextix.jardine.database.records.ActivityRecord;
+import co.nextix.jardine.database.records.CustomerRecord;
 import co.nextix.jardine.database.records.JDIproductStockCheckRecord;
+import co.nextix.jardine.database.records.PicklistRecord;
+import co.nextix.jardine.database.records.ProductRecord;
 
 import com.dd.CircularProgressButton;
 
@@ -26,21 +35,48 @@ public class AddJDIProductStockFragment extends Fragment {
 	private FragmentTransaction ft;
 	private Fragment fragmentForTransition;
 	private boolean flag = false;
-	
+
 	private Bundle bundle;
-	
 	private int frag_layout_id = 0;
+
+	private ArrayAdapter<ActivityRecord> activityAdapter = null;
+	private ArrayAdapter<ProductRecord> productAdapter = null;
+	private ArrayAdapter<PicklistRecord> stockStatusAdapter = null;
+	private ArrayAdapter<CustomerRecord> supplierAdapter = null;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		List<ActivityRecord> activityList = JardineApp.DB.getActivity().getAllRecords();
+		List<ProductRecord> productList = JardineApp.DB.getProduct().getAllRecords();
+		List<PicklistRecord> stockStatusList = JardineApp.DB.getJDIproductStatus().getAllRecords();
+		List<CustomerRecord> supplierList = JardineApp.DB.getCustomer().getAllRecords();
+
+		String assignedToFname = JardineApp.DB.getUser().getCurrentUser().getFirstNameName();
+		String assignedToLname = JardineApp.DB.getUser().getCurrentUser().getLastname();
 
 		final View view = inflater.inflate(R.layout.fragment_activity_add_jdi_product_stock_check, container, false);
-		
-		bundle = getArguments();
-		
-		if(bundle != null){
-			frag_layout_id = bundle.getInt("layoutID");
+		this.bundle = getArguments();
+
+		if (this.bundle != null) {
+			this.frag_layout_id = this.bundle.getInt("layoutID");
 		}
+
+		this.activityAdapter = new ArrayAdapter<ActivityRecord>(JardineApp.context, R.layout.add_activity_textview, activityList);
+		this.productAdapter = new ArrayAdapter<ProductRecord>(JardineApp.context, R.layout.add_activity_textview, productList);
+		this.stockStatusAdapter = new ArrayAdapter<PicklistRecord>(JardineApp.context, R.layout.add_activity_textview, stockStatusList);
+		this.supplierAdapter = new ArrayAdapter<CustomerRecord>(JardineApp.context, R.layout.add_activity_textview, supplierList);
+
+		((Spinner) view.findViewById(R.id.activity)).setAdapter(this.activityAdapter);
+		((Spinner) view.findViewById(R.id.product)).setAdapter(this.productAdapter);
+		((Spinner) view.findViewById(R.id.stock_status)).setAdapter(this.stockStatusAdapter);
+		((Spinner) view.findViewById(R.id.supplier)).setAdapter(this.supplierAdapter);
+		((TextView) view.findViewById(R.id.created_by)).setText(assignedToLname + "," + assignedToFname);
+
+		// Disable fields
+		((Spinner) view.findViewById(R.id.activity)).setEnabled(false);
+		((Spinner) view.findViewById(R.id.activity)).setClickable(false);
+		((TextView) view.findViewById(R.id.created_by)).setClickable(false);
+		((TextView) view.findViewById(R.id.created_by)).setEnabled(false);
 
 		((CircularProgressButton) view.findViewById(R.id.btnWithText1)).setOnClickListener(new OnClickListener() {
 
@@ -49,7 +85,7 @@ public class AddJDIProductStockFragment extends Fragment {
 				if (((CircularProgressButton) v).getProgress() == 0) {
 
 					ValueAnimator widthAnimation = ValueAnimator.ofInt(1, 100);
-					widthAnimation.setDuration(1500);
+					widthAnimation.setDuration(500);
 					widthAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
 					widthAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 						@Override
@@ -67,22 +103,30 @@ public class AddJDIProductStockFragment extends Fragment {
 
 					widthAnimation.start();
 
-					long stockStatus = ((JDIproductStockCheckRecord) ((Spinner) view.findViewById(R.id.stock_status)).getSelectedItem()).getId();
+					long product = ((ProductRecord) ((Spinner) view.findViewById(R.id.product)).getSelectedItem()).getId();
+					long stockStatus = ((JDIproductStockCheckRecord) ((Spinner) view.findViewById(R.id.stock_status)).getSelectedItem())
+							.getId();
 					long loadedOnShelves = ((CheckBox) view.findViewById(R.id.loaded_on_shelves)).isChecked() ? 1 : 0;
-					String otherTypeRemarks = ((EditText) view.findViewById(R.id.other_type_remarks)).getText().toString();
+					long supplier = ((CustomerRecord) ((Spinner) view.findViewById(R.id.supplier)).getSelectedItem()).getId();
+					long activity = ((ActivityRecord) ((Spinner) view.findViewById(R.id.activity)).getSelectedItem()).getId();
 
+					String otherTypeRemarks = ((EditText) view.findViewById(R.id.other_type_remarks)).getText().toString();
 					SharedPreferences pref = getActivity().getApplicationContext().getSharedPreferences("ActivityInfo", 0);
 
-					if (otherTypeRemarks != null && !otherTypeRemarks.isEmpty()) {
-						
-						if(AddActivityFragment.ACTIVITY_TYPE == 4){
+					if (product != 0 && stockStatus != 0) {
+
+						if (AddActivityFragment.ACTIVITY_TYPE == 4) {
 							fragmentForTransition = new AddActivityProductSupplierFragment();
-						}						
-						
+						}
+
 						Editor editor = pref.edit();
 						editor.putString("other_type_remarks", otherTypeRemarks);
 						editor.putLong("stock_status", stockStatus);
 						editor.putLong("loaded_on_shelves", loadedOnShelves);
+						editor.putLong("product", product);
+						editor.putLong("supplier", supplier);
+						editor.putLong("activity", activity);
+
 						editor.commit(); // commit changes
 					} else {
 						flag = false;
@@ -100,9 +144,9 @@ public class AddJDIProductStockFragment extends Fragment {
 					}
 				} else {
 					((CircularProgressButton) v).setProgress(0);
-					
+
 					fragmentForTransition.setArguments(bundle);
-					
+
 					ft = getActivity().getSupportFragmentManager().beginTransaction();
 					ft.replace(frag_layout_id, fragmentForTransition);
 					ft.addToBackStack(null);
