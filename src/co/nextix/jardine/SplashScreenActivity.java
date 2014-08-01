355a218e74a3d577598f315f773ac379adf6a121
carl <carl@nextixsystems.com>
2014-08-01 17:26:49 +0800

    splash changes

--- src/co/nextix/jardine/SplashScreenActivity.java
+++ src/co/nextix/jardine/SplashScreenActivity.java
@@ -3,12 +3,14 @@ package co.nextix.jardine;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SplashScreenActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.splash_screen);
 
 		try { // Simulate a long loading process on app startup.
